8753d2841b09370bf35843ff79d5adde36b39e17
SindiKali <m4ttpizz4@gmail.com>
2026-05-08 18:04:23 -0300

    Add files via upload

--- honeypot.py
+++ honeypot.py
@@ -6,14 +6,33 @@ logging.basicConfig(filename='capturas_honeypot.txt', level=logging.INFO, format
 
 def iniciar_honeypot(porta=2222):
     servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+    
     servidor.bind(('0.0.0.0', porta))
     servidor.listen(5)
-
-    print(f"Terminal de defesa ativo. Honeypot escutando silencionamente na porta {porta} ...")
-
+    
+    print(f"[+] Terminal de Defesa Ativo. Honeypot escutando silenciosamente na porta {porta}...")
+    
     while True:
         try:
             conexao, endereco = servidor.accept()
             ip_atacante = endereco[0]
-            hora_ataque = datetime.now().strtime("%Y-%m-%d %H:%M:%S")
+            hora_ataque = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            
+            print(f"[!] Alerta: Conexão detectada de: {ip_atacante}")
+
+            conexao.send(b"SSH-2.0-OpenSSH_7.2p2 Ubuntu-4ubuntu2.8\n")
+            
+            dados = conexao.recv(1024)
+            payload = dados.decode('utf-8', errors='ignore').strip()
+            
+            registro = f"[{hora_ataque}] Invasor: {ip_atacante} | Payload Tentado: {payload}"
+            logging.info(registro)
+            print(f"[*] Dados capturados e gravados no log de segurança.")
+            
+            conexao.close()
+            
+        except Exception as erro:
+            print(f"Erro no módulo de defesa: {erro}")
+
+if __name__ == "__main__":
+    iniciar_honeypot()
